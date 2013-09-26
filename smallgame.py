fa04635174e84dbb81255c27c2e1bdd8b3b8e4f9
John Carroll <jmcarroll95@gmail.com>
2013-09-25 21:50:37 -0400

    Simple Dungeon
    A challenge to your brain, because this is so bad.

---
+++ smallgame.py
@@ -0,0 +1,26 @@
+print "WELCOME TO DUNGEON KNOCK OFF!"
+import time
+
+time.sleep(1)
+
+user_name = raw_input("Enter your name: ")
+
+time.sleep(1)
+
+print "So... you're name is {name}?".format(name = user_name)
+
+raw_input("Y/N: ")
+
+x = 0
+while x < 3:
+    time.sleep(1)
+    print ".",
+    x = x + 1
+
+    
+print
+print "Ahh! You're name is Eugene! Wonderful!"
+
+time.sleep(2)
+print
+print "Well, Eugene, you are here to test your wits! To challenge your brain! To think, not just type in nonsense!"
