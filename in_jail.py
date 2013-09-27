a34b9ee93e42537f77f1f3ab4cb12f757fd0ea4b
NoahEM <noahem009@gmail.com>
2013-09-27 00:25:55 -0400

    jail scene

---
+++ in_jail.py
@@ -0,0 +1,83 @@
+
+import time
+
+def next():
+    time.sleep(1)
+    raw_input()
+
+def wait():
+    for x in range (0, 3):
+        print ".",
+        time.sleep(0.5)
+    print "\n",
+
+print "mumble mumble.........",
+next()
+print "Why is this guy here?.........",
+next()
+print "I'm not responsible for this.......",
+next()
+print "shuffle shuffle.......",
+next()
+print "*SLAM*",
+next()
+wait()
+print "You wake feeling cold",
+next()
+print "Would you like to look around?"
+print "Y/N:",
+next()
+wait()
+print "you are in a room"
+print "To the front of you is a toilet"
+print "To your right is a small pinhole of a window-"
+print "-you can berely see what looks like a fence"
+print "To your left are Iron bars blocking a hallway",
+next()
+print "You are in a jail cell",
+next()
+print "Someone is walking down the hallway",
+next()
+wait()
+print "The warden slides a tray under the bars",
+next()
+print "The tray has a Roast Beef sandwich on it",
+next()
+print "Eat the sandwich?"
+print "Y/N:",
+next()
+wait()
+print "You take a bite out of the sandwich and-"
+print "-crack one of your front teeth on a key which was inside",
+next()
+print "This is a bittersweet moment now since your smile is ruined for life",
+next()
+print "'sucks even more too since you have such great dental hygene",
+next()
+print "Terrific in fact",
+next()
+print "Your teeth were so white",
+next()
+print "But that doesn't matter now, they're ruined",
+next()
+wait()
+print "You pick up the key which has ruined your teeth,-"
+print "-it is covored in roastbeef sandwich",
+next()
+print "Would you like to try the key in the cell door?"
+print "Y/N:",
+next()
+wait()
+print "it fits!",
+next()
+print "Would you like to escape jail?"
+print "Y/N:",
+next()
+wait()
+print "You slide open the door and start down the hall",
+next()
+print "You greet the Warden as you walk out of the jail",
+next()
+print "'Hi' he says back to you"
+next()
+wait()
